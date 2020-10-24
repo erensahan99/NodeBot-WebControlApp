fd22c760fc1458b365d71cdaafe16ac516df6512
erensahan99 <eren.sahan99@gmail.com>
2020-10-24 20:31:28 +0300

    Added new feautes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,11 +14,23 @@ window.onload = function () {
         console.log('Got announcement:', data.message);
     });
 
+    socket.on('setup', function (data) {
+        document.getElementById("motor0").value = data.servo0
+        document.getElementById("motor1").value = data.servo1
+        document.getElementById("motor2").value = data.servo2
+        document.getElementById("kiskac").value = data.servo3
+    });
+
+    socket.on('movement', function (data) {
+        console.log(data);
+
+        document.getElementById(data.id).value = data.value
+    });
 
 }
 const abc = function(value){
     socket.emit('movement', {
         id: value.id, value: value.value
     });
-    console.log(value.id + ' ' + value.value);
+    // console.log(value.id + ' ' + value.value);
 }
