f883e3600f82ea5542de0a06f7cff34302eb3b94
Eren <eren.sahan99@gmail.com>
2021-01-03 09:18:18 +0300

    ✔ Added controller structure ✖ Removed old unnecessary files
    	deleted:    src/public/javascripts/threejsapp.js
    	deleted:    src/public/stl-files/deneme.stl
    	deleted:    src/views/chat.html
    	deleted:    src/views/robot-arm.pug
    	renamed:    src/views/rc-arac.pug -> src/views/robotControl.pug
    	deleted:    src/views/spot-kontrol.pug
    	deleted:    src/views/spot-nano.pug

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var mainRouter = require('./routes/index');
 
 var app = express();
 
@@ -17,23 +17,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/build/', express.static(path.join(__dirname, '../node_modules/three/build')));
-app.use('/jsm/', express.static(path.join(__dirname, '../node_modules/three/examples/jsm')));
-//app.use('/stlloader/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-app.use('/', indexRouter);
-app.use('/spot-nano', indexRouter);
-app.use('/spot-kontrol', indexRouter);
-app.use('/robot-arm', indexRouter);
-app.use('/rc-arac', indexRouter);
+app.use('/', mainRouter);
+app.use('/robot-control', mainRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
